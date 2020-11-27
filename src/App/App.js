4f2cfaabb83aa0d60c336a8e2e29cb887128c9bd
GrafSoul <grafsoul@gmail.com>
2020-11-27 16:35:44 +0500

    feat: Added MediaRecorder starter files

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+
+import MediaRecorder from './components/MediaRecorder/MediaRecorder';
+
 import './App.scss';
 
 class App extends Component {
     render() {
-        return <div className="App">Reco</div>;
+        return (
+            <div className="App">
+                <MediaRecorder />
+            </div>
+        );
     }
 }
 
---
+++ src/App/components/ListRecords/ListRecords.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ListRecords = ({ list, deleteEntry }) => {
+    return (
+        <ul className="list-media">
+            {list.map((item, index) => {
+                return (
+                    <li key={index}>
+                        {item}
+                        <button
+                            className="btn btn-danger"
+                            onClick={() => deleteEntry(index)}
+                        >
+                            X
+                        </button>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+};
+
+export default ListRecords;
