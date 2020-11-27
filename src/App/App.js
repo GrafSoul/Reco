c4f35aae620ac7f78dbb207e2a03963305cf3e8d
GrafSoul <grafsoul@gmail.com>
2020-11-27 12:56:45 +0500

    feat: Added starter files for the project.

---
+++ src/App/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return <div className="App">Reco</div>;
+    }
+}
+
+export default App;
