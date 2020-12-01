0e43c2fbf2ad2681bb594f82a1ebbf7210ed7be6
GrafSoul <grafsoul@gmail.com>
2020-12-01 17:49:20 +0500

    feat: Made minor corrections to MediaRecorder.js

--- src/App/components/MediaRecorder/MediaRecorder.js
+++ src/App/components/MediaRecorder/MediaRecorder.js
@@ -294,27 +294,33 @@ const MediaRecorder = () => {
                     ></video>
                 </div>
 
-                <div className="settings">
-                    <h2>
-                        Configure your {onlyAudio ? null : 'camera and '} audio
-                        devices
-                    </h2>
-                    <div
-                        className={[
-                            'video-settings',
-                            onlyAudio ? 'invisible' : null,
-                        ].join(' ')}
-                    >
-                        <div className="select">
-                            <label htmlFor="videoSource">Video source:</label>
-                            <select id="videoSource" ref={videoSelect}></select>
-                        </div>
-                    </div>
+                <AudioMeter />
 
+                <div className="settings">
                     <div className="audio-settings">
+                        <h2>
+                            Configure your {onlyAudio ? null : 'camera and '}{' '}
+                            audio devices
+                        </h2>
+                        <div
+                            className={[
+                                'video-settings',
+                                onlyAudio ? 'invisible' : null,
+                            ].join(' ')}
+                        >
+                            <div className="select">
+                                <label htmlFor="videoSource">
+                                    Video source:
+                                </label>
+                                <select
+                                    id="videoSource"
+                                    ref={videoSelect}
+                                ></select>
+                            </div>
+                        </div>
                         <div className="select">
                             <label htmlFor="audioSource">Audio input:</label>
-                            <AudioMeter />
+
                             <select
                                 id="audioSource"
                                 ref={audioInputSelect}
